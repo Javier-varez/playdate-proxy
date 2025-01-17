ef870df0396477fef281e4500c93a0e920d14a71
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2025-01-18 00:56:48 +0100

    Finish implementation of API handler

--- src/handlers/api.zig
+++ src/handlers/api.zig
@@ -44,13 +44,57 @@ fn resolveApi(value: anytype, path: []const []const u8) ?*const anyopaque {
     }
 }
 
-fn handler(app_state: *AppState, args: [][]const u8) void {
-    if (resolveApi(app_state.pd, args)) |ptr| {
-        app_state.print("Ptr is {}", .{ptr});
+fn readData(app_state: *AppState, _ptr: *const anyopaque, length: usize) !std.ArrayList(u8) {
+    var encoded_binary_blob = std.ArrayList(u8).init(app_state.allocator.allocator());
+    errdefer encoded_binary_blob.deinit();
+
+    const encoded_size = std.base64.standard.Encoder.calcSize(length);
+    try encoded_binary_blob.resize(encoded_size);
+
+    const ptr: [*]const u8 = @ptrCast(_ptr);
+    _ = std.base64.standard.Encoder.encode(encoded_binary_blob.items, ptr[0..length]);
+
+    return encoded_binary_blob;
+}
+
+fn handler(app_state: *AppState, args: []const []const u8) void {
+    if (args.len < 1) {
+        app_state.print("Err: Missing arguments to API handler", .{});
         return;
     }
 
-    app_state.print("Unknown method {s}", .{args});
+    const encoded_len_size = comptime std.base64.standard.Encoder.calcSize(@sizeOf(u32));
+    if (args[0].len != encoded_len_size) {
+        app_state.print("Err: Unexpected length argument size. Got {}, Expected {}", .{ args[0].len, encoded_len_size });
+        return;
+    }
+
+    var decoded_length: [@sizeOf(u32)]u8 = undefined;
+    _ = std.base64.standard.Decoder.decode(&decoded_length, args[0]) catch {
+        app_state.print("Err: Unable to decode length argument", .{});
+        return;
+    };
+    const length: u32 = @bitCast(decoded_length);
+
+    const ptr = resolveApi(app_state.pd, args[1..]);
+    if (ptr == null) {
+        app_state.print("Err: Unknown API method {s}", .{args});
+        return;
+    }
+
+    const encoded_ptr_size = comptime std.base64.standard.Encoder.calcSize(@sizeOf(*const anyopaque));
+    const src_ptr_data: [@sizeOf(*const anyopaque)]u8 = @bitCast(@intFromPtr(ptr.?));
+    var encoded_ptr_data: [encoded_ptr_size]u8 = undefined;
+    _ = std.base64.standard.Encoder.encode(&encoded_ptr_data, &src_ptr_data);
+
+    const MASK: usize = 0xFFFFFFFE;
+    const encoded_binary_blob = readData(app_state, @ptrFromInt(@intFromPtr(ptr.?) & MASK), length) catch {
+        app_state.print("Err: failed to read data at address {s}", .{encoded_ptr_data});
+        return;
+    };
+    defer encoded_binary_blob.deinit();
+
+    app_state.print("{s} {s}", .{ encoded_ptr_data, encoded_binary_blob.items });
 }
 
 pub fn register() !void {
@@ -82,6 +126,21 @@ test "resolveApi" {
 
     {
         const ptr: ?*const anyopaque = resolveApi(&api, &.{ "system", "formatStrings" });
+        try std.testing.expectEqual(ptr, null);
+    }
+
+    {
+        const ptr: ?*const anyopaque = resolveApi(&api, &.{ "system", "formatString", "more" });
+        try std.testing.expectEqual(ptr, null);
+    }
+
+    {
+        const ptr: ?*const anyopaque = resolveApi(&api, &.{"system"});
+        try std.testing.expectEqual(ptr, null);
+    }
+
+    {
+        const ptr: ?*const anyopaque = resolveApi(&api, &.{});
         try std.testing.expectEqual(ptr, null);
     }
 }
--- src/serial.zig
+++ src/serial.zig
@@ -4,7 +4,7 @@ const PdApi = @import("pd_api.zig").Api;
 const AppState = @import("app_state.zig");
 const Handlers = @import("handlers.zig");
 
-const Handler = *const fn (app_state: *AppState, args: [][]const u8) void;
+const Handler = *const fn (app_state: *AppState, args: []const []const u8) void;
 
 const Command = struct {
     name: []const u8,
@@ -39,19 +39,18 @@ fn serial_callback(message: [*:0]const u8) callconv(.C) void {
         if (v.len == 0) continue;
 
         args.append(v) catch {
-            app_state.print("Out of memory while collecting args for command", .{});
+            app_state.panic("Err: Out of memory while collecting args for command", .{});
             return;
         };
     }
 
     if (args.items.len == 0) {
-        app_state.print("Please, specify a command to handle", .{});
+        app_state.print("Err: Please, specify a command to handle", .{});
         return;
     }
 
     const cmd = args.items[0];
     const other_args = args.items[1..];
-
     for (serial_state.?.commands.items) |c| {
         if (std.mem.eql(u8, cmd, c.name)) {
             c.handler(app_state, other_args);
@@ -59,7 +58,7 @@ fn serial_callback(message: [*:0]const u8) callconv(.C) void {
         }
     }
 
-    app_state.print("Unknown command {s}", .{cmd});
+    app_state.print("Err: Unknown command {s}", .{cmd});
 }
 
 pub fn register_command(command: []const u8, handler: Handler) !void {
